f2b02bdc73c38915056c75852e3c66fce3e66f70
Shashank M Hegde <35756376+Shashankmhegde@users.noreply.github.com>
2019-09-14 12:17:49 +0530

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -2,18 +2,17 @@ import React from 'react'
 import { Text,Image ,StyleSheet} from 'react-native'
 import NavBar, { NavTitle, NavButton } from 'react-native-nav'
 import styles from 'react-native-nav/styles';
-
+const U = 'User'
 const ac='active Now'
 
 export default function NavBarCustom() {
   return (
     <NavBar style={styles.navBarAndroid}>
-      <Image source={require('./images.png')}
-          
+      <Image source={require('./iconfinder_React.js_logo_1174949.png')}
           style={{ width: 50, height: 50, borderRadius: 50 / 2 }}
         />
       <NavTitle>
-      User{'\n'}
+     <Text> {U}</Text>{'\n'}
         <Text style={{ fontSize: 10, color: '#0EB030'}}>
           {ac}
         </Text>
